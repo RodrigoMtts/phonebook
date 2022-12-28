03458cf1ad664af39e6d7b054f828ef3307374fa
Rodrigo da Matta Soares <rodrigo.matta.soares@gmail.com>
2022-12-28 03:12:55 -0300

    part3, phonebook, step 3.2

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (require,response) => {
     response.json(persons)
 })
 
+app.get('/info', (request,response) => {
+    const personsCount = persons.length
+    const date = new Date()
+    //response.set({ 'content-type': 'charset=utf-8' })
+    response.end(
+        `Phonebook has info for ${personsCount} people
+        
+${date}`
+    )
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
